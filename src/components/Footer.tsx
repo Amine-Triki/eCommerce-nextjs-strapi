8ec1ab59f53ad141cd99a516c0bde9bf26e5a538
Your Name <amine.triki15@gmail.com>
2025-11-08 23:34:26 +0100

    Add Footer component and include it in the layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center px-4 py-3 border-t bg-background text text-muted-foreground">
+      © 2025 - {new Date().getFullYear()} MAT Store ™ -- create by&nbsp;
+      <Link
+        href="https://aminetriki.com.tn"
+        className="text-blue-500 hover:text-blue-800 transition-colors duration-200"
+        target="_blank"
+      >
+        Amine Triki
+      </Link>
+    </div>
+  );
+};
+
+export default Footer;
